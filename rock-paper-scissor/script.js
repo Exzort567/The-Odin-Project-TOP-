4af0a71565e5d9f413a6f705af540aab22f06437
Kenneth <quibelkenneth@gmail.com>
2024-05-17 21:43:33 +0800

    Completed the backend for the game

--- rock-paper-scissor/script.js
+++ rock-paper-scissor/script.js
@@ -6,3 +6,63 @@ function getComputerChoice() {
 function getHumanChoice() {
     return prompt("Please enter 'rock', 'paper', 'scissors: '");
 }
+
+
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    
+    const humanSelection = function () { return getHumanChoice().toUpperCase(); }
+    const computerSelection = function () { return getComputerChoice(); }
+
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            console.log('TIE');
+        } else if (humanChoice === 'ROCK' && computerChoice === 'PAPER') {
+            computerScore++;
+            console.log('You loose! Paper beats Rock.');
+        } else if (humanChoice === 'PAPER' && computerChoice ==='ROCK') {
+            humanScore++;
+            console.log('You win! Paper beats Rock.');
+        } else if (humanChoice === 'PAPER' && computerChoice ==='SCISSORS') {
+            computerScore++;
+            console.log('You loose! Scissors beat Paper');
+        } else if (humanChoice === 'SCISSORS' && computerChoice === 'PAPER') {
+            humanScore++;
+            console.log('You win! Scissors beats Paper');
+        } else if (humanChoice === 'ROCK' && computerChoice === 'SCISSORS') {
+            humanScore++;
+            console.log('You win! Rock beats Scissors.');
+        } else if (humanChoice === 'SCISSORS' && computerChoice === 'ROCK') {
+            computerScore++;
+            console.log('You loose! Rock beats Scissors.');
+        } else {
+            console.log(humanChoice + ' is not a correct value. Please enter ROCK, PAPER, SCISSORS')
+        }
+
+    }
+    
+
+    for (i = 1; i <= 5; i++) {
+        console.log('Game: ' + i);
+        playRound(humanSelection(), computerSelection());
+    }
+
+    if (humanScore > computerScore) {
+        console.log('You win! The total score is ' + humanScore + ' ' + computerScore);
+    } else {
+        console.log('You loose! The total score is ' + computerScore + ' ' + humanScore);
+    }
+
+}
+
+playGame();
+
+
+
+
+
+
