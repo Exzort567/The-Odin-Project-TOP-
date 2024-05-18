519e8b4599f3222da42a3da47a818e19eb75b537
Kenneth <quibelkenneth@gmail.com>
2024-05-18 21:10:04 +0800

    TOP javascript exercise number 2

---
+++ repeat-string/repeatString.js
@@ -0,0 +1,11 @@
+const repeateString = function() {
+    str = 'hey';
+    result = '';
+  
+      for (i = 1; i < 4; i++) {
+          result += str;
+      }
+      console.log(result);
+  };
+  
+  repeateString();
