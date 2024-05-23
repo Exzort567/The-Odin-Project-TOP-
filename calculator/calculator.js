4fd8af2feddfca090e979829c085053542535122
Kenneth <quibelkenneth@gmail.com>
2024-05-23 09:30:16 +0800

    TOP javascript exercise no.8

---
+++ calculator/calculator.js
@@ -0,0 +1,39 @@
+const add = function(a, b) {
+    return a + b;
+	
+};
+
+const subtract = function(a ,b) {
+    return a - b;
+};
+
+const sum = function(arr) {
+	return arr.reduce((total, curr) => total + curr, 0)
+};
+
+const multiply = function(arr) {
+    return arr.reduce((total, curr) => total * curr, 0)
+};
+
+const power = function(a, b) {
+	return Math.pow(a,b);
+};
+
+const factorial = function(n) {
+	if (n === 0) return 1;
+    let product = 1;
+    for (i = n; i > 0; i--) {
+        product *= i;
+    }
+    return product;
+};
+
+// Do not edit below this line
+module.exports = {
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial
+};
